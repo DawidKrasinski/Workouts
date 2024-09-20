3955cce1f22326bff6a1a104be00367c7bd08e5b
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-09-20 21:49:03 +0200

    improved layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 export default function Home() {
-  const Day = (props) => {
+  const Day = () => {
     return (
-    <div style={{width: '270px', height: '350px'}} className="bg-white border-2 border-gray-400 rounded-xl "></div>
+    <div style={{width: '270px', height: '350px'}} className="bg-white border-2 border-gray-400 rounded-xl"></div>
   )}
 
   return (
-<body style={{height: '1700px', gridTemplateColumns: '1fr 450px 50px 450px 1fr', gridTemplateRows: '50px 500px 50px 500px 50px 500px'}}
+<div style={{height: '1700px', gridTemplateColumns: '1fr 450px 50px 450px 1fr', gridTemplateRows: '50px 500px 50px 500px 50px 500px'}}
  className="bg-gray-200 flex justify-center grid">
     <header style={{gridColumn: '2 / span 3', gridRowStart: '2'}} className="border-2 border-gray-400 rounded-3xl bg-gray-100 shadow-2xl shadow-gary-900 flex justify-around flex-wrap">
       <span style={{marginTop: '20px'}} className="text-5xl font-semibold">PLAN TRENINGOWY
@@ -17,15 +17,17 @@ export default function Home() {
         <Day/>
       </div>
     </header>
-    <section style={{gridColumnStart: '2', gridRowStart: '4'}} className="border-2 border-gray-400 rounded-3xl bg-gray-100 shadow-2xl shadow-gary-900 flex justify-center">
+    <section style={{gridColumnStart: '2', gridRowStart: '4'}} className="border-2 border-gray-400 rounded-3xl bg-gray-100 shadow-2xl shadow-gary-900 flex justify-center flex-wrap">
     <span style={{marginTop: '20px'}} className="text-5xl font-semibold">WYNIKI
         <a href="/">(+)</a>
       </span>
+      <div style={{height: '370px', width: '350px'}} className="bg-white border-2 border-gray-400 rounded-2xl"></div>
     </section>
-    <section style={{gridColumnStart: '4', gridRowStart: '4'}} className="border-2 border-gray-400 rounded-3xl bg-gray-100 shadow-2xl shadow-gary-900 flex justify-center">
+    <section style={{gridColumnStart: '4', gridRowStart: '4'}} className="border-2 border-gray-400 rounded-3xl bg-gray-100 shadow-2xl shadow-gary-900 flex justify-center flex-wrap">
     <span style={{marginTop: '20px'}} className="text-5xl font-semibold">CELE
         <a href="/">(+)</a>
       </span>
+      <div style={{height: '370px', width: '350px'}} className="bg-white border-2 border-gray-400 rounded-2xl"></div>
     </section>
 
     <footer style={{gridColumn: '2 / span 3', gridRowStart: '6'}} className="border-2 border-gray-400 rounded-3xl bg-gray-100 shadow-2xl shadow-gary-900 flex justify-center">
@@ -33,6 +35,6 @@ export default function Home() {
         <a href="/">(+)</a>
       </span>
     </footer>
-</body>
+</div>
   );
 }
