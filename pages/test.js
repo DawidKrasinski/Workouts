e723708f95489f38ffe112eb5af3fbcb016790d3
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-09-21 20:50:16 +0200

    test - polymorphic

--- pages/test.js
+++ pages/test.js
@@ -1,32 +1,37 @@
 export default function Test() {
-  class auto {
-    constructor(mark, year, model) {
-      this.mark = mark
-      this.year = year
-      this.model = model
-    }
 
-    Opis () {
-      return <span>{this.model} {this.mark} {this.year}<br/></span>
+  class animal {
+    constructor (name) {
+      this.name = name
+    }
+    sound() {
+      return 'dzwiek zwierzecia'
     }
   }
 
-  class truck extends auto {
-    constructor(mark, year, model, type) {
-      super(mark, year, model)
-      this.type = type
-    }
-    Opis () {
-      return <span>{this.type} {this.model} {this.mark} {this.year}<br/></span>
+  class dog extends animal {
+    sound(){
+      return <span>{this.name} szczeka: Hau Hau!<br/></span>
     }
   }
 
-  const newAuto = new auto ('Toyota', '2022', 'corolla')
-  const newTruck = new truck('motobike', 'x', '2020', 'y')
-   
-  return (
-  <div>
-    {newAuto.Opis()}{newTruck.Opis()}
-  </div>
-  )
+  class owczarek extends dog {
+    constructor(name, rasa) {
+      super(name)
+      this.rasa = rasa
+    }
+  }
+
+  class cat extends animal {
+    sound(){
+      return <span>{this.name} miauczy: Miau Miau!<br/></span>
+    }
+  }
+
+  const burek = new dog ('Burek')
+  const mruczek = new cat ('Mruczek')
+  const azor = new owczarek ('Azor', 'owczarek')
+
+  return <span>{burek.sound()}{mruczek.sound()}{azor.sound()}</span>
 }
+
