046cf1066693a2cf918ec02e9969c160ec47b3b2
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-09-21 20:25:38 +0200

    test - class

--- pages/test.js
+++ pages/test.js
@@ -7,11 +7,26 @@ export default function Test() {
     }
 
     Opis () {
-      return <span>{this.model} {this.mark} {this.year}</span>
+      return <span>{this.model} {this.mark} {this.year}<br/></span>
+    }
+  }
+
+  class truck extends auto {
+    constructor(mark, year, model, type) {
+      super(mark, year, model)
+      this.type = type
+    }
+    Opis () {
+      return <span>{this.type} {this.model} {this.mark} {this.year}<br/></span>
     }
   }
 
   const newAuto = new auto ('Toyota', '2022', 'corolla')
+  const newTruck = new truck('motobike', 'x', '2020', 'y')
    
-  return <div>{newAuto.Opis()}</div>;
+  return (
+  <div>
+    {newAuto.Opis()}{newTruck.Opis()}
+  </div>
+  )
 }
