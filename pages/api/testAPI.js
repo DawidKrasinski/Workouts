3557b2b71ce89679130d137664667eccc4675b10
DawidKrasinski <dawidkrasinski14@gmail.com>
2024-09-28 19:58:05 +0200

    test - backend

--- pages/api/testAPI.js
+++ pages/api/testAPI.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({message: 'hello from next js api!'})
+    res.status(200).json({message: 'hello from backend!'})
 }
--- pages/test.js
+++ pages/test.js
@@ -1,14 +1,19 @@
 import { useEffect } from "react"
+import { useState } from "react"
 
 export default function Test() {
+
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     fetch('api/testAPI')
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => setMessage(data.message))
       .catch((error) => console.log(error))
   })
 
-  return <div>1</div>
+  return <div>
+    <p>message from backend: {message} </p>
+  </div>
 }
 
